6cc57dd19cd143ac54a690845138fdb248c11988
chyu <u.65l4cjo@gmail.com>
2019-04-21 23:14:59 -0700

    p2

--- nachos/threads/KThread.java
+++ nachos/threads/KThread.java
@@ -407,6 +407,29 @@ public class KThread {
 		private int which;
 	}
 
+	private static void joinTest1 () {
+		KThread child1 = new KThread( new Runnable () {
+			public void run() {
+				System.out.println("I (heart) Nachos!");
+			}
+			});
+		child1.setName("child1").fork();
+	
+		// We want the child to finish before we call join.  Although
+		// our solutions to the problems cannot busy wait, our test
+		// programs can!
+	
+		for (int i = 0; i < 5; i++) {
+			System.out.println ("busy...");
+			KThread.currentThread().yield();
+		}
+	
+		child1.join();
+		System.out.println("After joining, child1 should be finished.");
+		System.out.println("is it? " + (child1.status == statusFinished));
+		Lib.assertTrue((child1.status == statusFinished), " Expected child1 to be finished.");
+		}
+
 	/**
 	 * Tests whether this module is working.
 	 */
