d3f3ef58c8e9cb87c95407c0b7560a56b53f22ae
Chaohui Yu <c1yu@ieng6-202.ucsd.edu>
2019-06-07 13:49:00 -0700

    add test cases

--- nachos/test/mv.c
+++ nachos/test/mv.c
@@ -8,35 +8,38 @@ char buf[BUFSIZE];
 
 int main(int argc, char** argv)
 {
-  int src, dst, amount;
+      int src, dst, amount;
 
-  argc = 3;
-  argv[1]
-  if (argc!=3) {
-    printf("Usage: cp <src> <dst>\n");
-    return 1;
-  }
+      if (argc!=3) {
+              printf("Usage: cp <src> <dst>\n");
+                  return 1;
 
-  src = open(argv[1]);
-  if (src==-1) {
-    printf("Open to open %s\n", argv[1]);
-    return 1;
-  }
+      }
 
-  creat(argv[2]);
-  dst = open(argv[2]);
-  if (dst==-1) {
-    printf("Unable to create %s\n", argv[2]);
-    return 1;
-  }
+        src = open(argv[1]);
+        if (src==-1) {
+                printf("Open to open %s\n", argv[1]);
+                    return 1;
 
-  while ((amount = read(src, buf, BUFSIZE))>0) {
-    write(dst, buf, amount);
-  }
+        }
 
-  close(src);
-  close(dst);
-  unlink(argv[1]);
+          creat(argv[2]);
+            dst = open(argv[2]);
+            if (dst==-1) {
+                    printf("Unable to create %s\n", argv[2]);
+                        return 1;
 
-  return 0;
+            }
+
+            while ((amount = read(src, buf, BUFSIZE))>0) {
+                    write(dst, buf, amount);
+
+            }
+
+              close(src);
+                close(dst);
+                  unlink(argv[1]);
+
+                    return 0;
+
 }
